5de60650a1b9506e457a907ce09b7e050d45bb35
pjagatap <prashanth120783@gmail.com>
2020-03-29 13:15:43 +0530

    This is my first commit to GIT to upload my first python program!!

---
+++ homeloanCalculator.py
@@ -0,0 +1,14 @@
+p = float (input('Enter the principal outstanding: '))
+R = float (input('Rate of interest: '))
+N = float (input('Enter number of months remaining: '))
+R=R/(12*100)
+E = (p*R*((1+R)**N))/((1+R)**(N)-1)
+print ('Your monthly EMI is:', E)
+print ("Amortization")
+#interest calculation
+while (N>0):
+    N-=1
+    i=p*R
+    pm=E-i
+    print(int(N), 'your monthly interest is',i, 'monthly principal is', pm)
+    p-=pm
