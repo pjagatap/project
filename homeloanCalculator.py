d3f0020bcf494931dd066dbd9fee773df07a6b72
Prashanth Jagatap <prashanthjagatap@gmail.com>
2020-09-22 20:19:03 +0530

    Fomatting the output to display amount in line.

--- homeloanCalculator.py
+++ homeloanCalculator.py
@@ -1,16 +1,18 @@
-#This program can gives you home loan EMI and also it's Amortization schedule
-
+#This is a piece of code to calculate Home Loan EMI & Amortization schedule.
 p = float (input('Enter the principal outstanding: '))
 R = float (input('Rate of interest: '))
 N = float (input('Enter number of months remaining: '))
 R=R/(12*100)
 E = (p*R*((1+R)**N))/((1+R)**(N)-1)
+total_interest = 0
 print ('Your monthly EMI is:', E)
 print ("Amortization")
-#interest calculation
-while (N>0):
+#Amortization schedule.
+while (N>1):
     N-=1
     i=p*R
+    total_interest += i
     pm=E-i
-    print(int(N), 'your monthly interest is | ',i, 'monthly principal is | ', pm)
+    print(f'{int(N):>3}, "your interest is:" {round(i,2):>8.2f}, "your principal is:" {round(pm,2):>8.2f}')
     p-=pm
+print(f'Total interest payable {total_interest:>8.2f}')
